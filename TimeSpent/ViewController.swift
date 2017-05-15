254c5f8d9903fbec06fda3b87e204ae774aafcbd
Rapier97 <alex.rapier13@gmail.com>
2017-05-15 14:28:21 +0100

    initial commit

---
+++ TimeSpent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeSpent
+//
+//  Created by Alex Rapier on 15/05/2017.
+//  Copyright © 2017 AlexRapier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
